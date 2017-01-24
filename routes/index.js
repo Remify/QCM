a8e62f638b334e3dc2e20ae2a912a10e461beeff
Rémi B <remibouguermouh@gmail.com>
2017-01-24 19:09:25 +0100

    Affichage et structure des questions

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var questions = [
+    {
+        id : 1,
+        titre : "Quelle est la couleur du cheval blanc d'Henry IV ?",
+        reponses: [
+            { id : 1, text : "Bleu" },
+            { id : 2, text : "Blanc" },
+            { id : 3, text : "Rouge"}
+        ]
+    },
+    {
+        id : 2,
+        titre : "Qu'es ce qui est petit et marron ?",
+        reponses:  [
+            { id : 1, text : "Un petit Rouge" },
+            { id : 2, text : "Un Marron" },
+            { id : 3, text : "Un petit Marron"}
+        ]
+    }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.io.emit("socketToMe", "Hello world");
-    res.render('index', {title : "Hello"});
+    res.io.emit("socketToMe", questions);
+    res.render('index', {questions : questions});
 });
 
 
