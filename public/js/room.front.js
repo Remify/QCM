61a2d68f9296124739bc01d25dcdc94fbcec2a86
remify <remibouguermouh@gmail.com>
2017-02-18 15:10:17 +0100

    fix bugs sur jquery pour envoie resultats

--- public/js/room.front.js
+++ public/js/room.front.js
@@ -4,16 +4,30 @@ var questions = $('.question');
 
 $(document).ready(function() {
     $('.question').each(function (question) {
-
+        var question = this;
         var questionDiv = this;
         $.ajax({ url: '/room/dev/question/' + this.dataset.id + '/getResponses'}).done(function (res) {
             console.log(res);
             res.reponses.forEach(function (reponse) {
                 var div = $("<div></div>")
-                var radio = $("<input type='radio' name='reponse' data-question-id='" + reponse.id +"'/>" );
+                var radio = $("<input type='radio' name='reponse'   value='" + reponse.id + "' data-question-id='" + question.dataset.id +"'/>" );
                 div.append(radio);
                 div.append(reponse.intitule);
                 $(questionDiv).find('form').first().append(div);
+
+                // Abonnement à change
+                $(radio).change(function() {
+                    console.log('change');
+                    var questionId = this.dataset.questionId;
+                    var data = {
+                        questionId : this.dataset.questionId,
+                        responseId : this.value
+                    }
+
+                    console.log(data);
+                    socket.emit("inputQuestion", data);
+
+                });
             })
         });
 
@@ -21,19 +35,6 @@ $(document).ready(function() {
     });
 
 
-    $('.reponse input[type=radio]').change(function() {
-        var questionId = this.dataset.questionId;
-
-        var data = {
-            questionId : this.dataset.questionId,
-            responseId : this.value
-        }
-
-
-        console.log(data);
-        socket.emit("inputQuestion", data);
-
-    });
 
 
     socket.on("hello", function (data) {
