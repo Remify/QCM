dff251ce0d7f2626e8f71697f239bfed0fd064c0
Remify <remi.bouguermouh@univ-grenoble-alpes.fr>
2017-02-22 13:13:49 +0100

    fix question en double + ajouts boutons

--- public/js/room.front.js
+++ public/js/room.front.js
@@ -21,35 +21,36 @@ $(document).ready(function() {
     socket.on("displayQuestion", function (data) {
 
         console.log(data);
+        if($(".question[data-id='" + data.id + "']").length == 0) {
 
-        var qDiv = $("<div class='question' data-id='" + data.id +"'></div>");
-        qDiv.append($("<div class='intitule'>" + data.intitule + "</div>"));
-        qDiv.append($("<div class='reponse'></div>").append($("<form></form>")));
+            var qDiv = $("<div class='question' data-id='" + data.id +"'></div>");
+            qDiv.append($("<div class='intitule'>" + data.intitule + "</div>"));
+            qDiv.append($("<div class='reponse'></div>").append($("<form></form>")));
 
-        data.reponses.forEach(function (reponse) {
-            var div = $("<div></div>")
-            var radio = $("<input type='radio' name='reponse'   value='" + reponse.id + "' data-question-id='" + data.id +"'/>" );
-            div.append(radio);
-            div.append(reponse.intitule);
-            qDiv.find('form').append(div);
+            data.reponses.forEach(function (reponse) {
+                var div = $("<div></div>")
+                var radio = $("<input type='radio' name='reponse'   value='" + reponse.id + "' data-question-id='" + data.id +"'/>" );
+                div.append(radio);
+                div.append(reponse.intitule);
+                qDiv.find('form').append(div);
 
-            // Abonnement à change
-            $(radio).change(function() {
-                var questionId = this.dataset.questionId;
-                var data = {
-                    questionId : this.dataset.questionId,
-                    responseId : this.value,
-                    user: registration.name
-                }
+                // Abonnement à change
+                $(radio).change(function() {
+                    var questionId = this.dataset.questionId;
+                    var data = {
+                        questionId : this.dataset.questionId,
+                        responseId : this.value,
+                        user: registration.name
+                    }
 
-                console.log(data);
-                socket.emit("inputQuestion", data);
+                    console.log(data);
+                    socket.emit("inputQuestion", data);
 
+                });
             });
-        });
 
-        $('.container').append(qDiv);
-        console.log(qDiv)
+            $('.container').append(qDiv);
+        }
 
     });
 
