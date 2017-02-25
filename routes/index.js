24d70cd7be287d2e7135081f88c0e44b1d9a46a6
remify <remibouguermouh@gmail.com>
2017-02-25 18:32:00 +0100

    admin login + DB Update

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,32 @@
 var express = require('express');
 var router = express.Router();
-var Question = require('../data/question')
-var questionDAO = require('../data/questionDAO')
 
+var AuthService = require('../services/auth.service')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
         res.render('index');
 });
 
+router.get('/login', function(req, res, next) {
+    res.render('admin/login');
+});
+
+router.post('/login/connect', function (req, res, next) {
+
+    AuthService.login(req.body.login, req.body.password, function (callback) {
+
+        if(callback.code == 200) {
+            req.session.loggedIn = true;
+            console.log(req.session)
+            res.redirect('/admin/')
+        } else {
+            req.session.loggedIn = false
+            res.redirect('/login?code=' + callback.code)
+        }
+
+    });
+})
 
 
 module.exports = router;
